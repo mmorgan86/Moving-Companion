0e5c5ab2cce4bee303400d0ad71714984ac67967
Michael Morgan <michael1986morgan@gmail.com>
2018-08-26 09:43:45 -0400

    updated to check if a value is entered in the street input field

--- js/script.js
+++ js/script.js
@@ -16,8 +16,16 @@ function loadData() {
     let cityStr = $('#city').val()
     let location = `${streetStr},${cityStr}`
 
-    $greeting.text(`So, you want to live at ${location} `)
+    // check to see if streetStr gets a value
+    if (streetStr == "") {
+         $greeting.text(`So, you want to live in ${cityStr}`)
+    } else {
+        $greeting.text(`So, you want to live at ${streetStr} ${cityStr} `)
+    }
 
+
+    // $greeting.text(`So, you want to live at ${streetStr} ${cityStr} `)
+    
     $body.append(`<img class="bgimg" src="${gMaprsUrl}${location}">`)
 
     // NEW YORK TIMES AJAX REQUEST
@@ -28,28 +36,28 @@ function loadData() {
         $nytHeaderElem.text(`New York Times Articles About ${cityStr}`)
 
         let articles = data.response.docs
-            articles.forEach(function (article) {
+        articles.forEach(function (article) {
             $nytElem.append(`<li class="article"> <a target="_blank" href="${article.web_url}">${article.headline.main}</a><p>${article.snippet}</p></li>`)
-            })
-    }).fail(function(e) {
-            $nytHeaderElem.text('New York Times Articles Could Not Be Loaded')
+        })
+    }).fail(function (e) {
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded')
     })
 
     // WIKIPEDIA AJAX REQUEST
     let wikiUrl = `http://www.wikipedia.org/w/api.php?action=opensearch&search=${cityStr}&format=json&callback=wikiCallback`
 
-    let wikiRequestTimeout = setTimeout(function() {
+    let wikiRequestTimeout = setTimeout(function () {
         $wikiElem.text("failed to get wikipedia resources")
     }, 8000)
 
     $.ajax({
-        url: wikiUrl, 
+        url: wikiUrl,
         dataType: 'jsonp',
         // jsonp: "callback",
-        success: function(response) {
+        success: function (response) {
             let articleList = response[1]
 
-            articleList.forEach(function(article){
+            articleList.forEach(function (article) {
                 let url = `http://en.wikipedia.org/wiki/wiki${article}`
                 $wikiElem.append(`<li><a href="${url}" target="_blank">${article}</a></li>`)
             })
